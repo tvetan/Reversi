3260fb394547da849c56eaecd3c098382a939251
tvetan <tvetan@gbg.bg>
2015-06-08 19:36:49 +0300

    Add enumeration for piece state

--- Reversi/piece.py
+++ Reversi/piece.py
@@ -1,6 +1,7 @@
 
 class Piece():
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, state):
         self.x = x
         self.y = y
+        self.state = state
---
+++ Reversi/state.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class State(Enum):
+    empty = 1
+    white = 2
+    black = 3
