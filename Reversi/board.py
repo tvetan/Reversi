d86ab2fae68b5fbd56ea9b4aa7d3f8c1a9a7a713
tvetan <tvetan@gbg.bg>
2015-06-08 20:07:55 +0300

    Update board to change piece state

--- Reversi/board.py
+++ Reversi/board.py
@@ -1,6 +1,25 @@
+from piece import Piece
+from state import State
+
 
 class Board():
 
     def __init__(self):
         self.width = 8
         self.height = 8
+        self.pieces = [
+            [Piece(x, y, State.empty) for y in range(self.width)]
+            for x in range(self.height)
+        ]
+
+    def make_white(self, x, y):
+        self.change_piece(x, y, State.white)
+
+    def make_black(self, x, y):
+        self.change_piece(x, y, State.black)
+
+    def make_empty(self, x, y):
+        self.change_piece(x, y, State.empty)
+
+    def change_piece(self, x, y, state):
+        self.pieces[x][y] = state
